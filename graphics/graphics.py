8603ff36cb71d4a7c87071d5913b245a01042218
kribrung <kribrung@stud.ntnu.no>
2019-12-06 01:34:55 +0100

    add random spawning of food pos. need fix to food pos calculation.

--- graphics/graphics.py
+++ graphics/graphics.py
@@ -9,25 +9,32 @@ def draw_snake(surface,Snake):
             pygame.draw.circle(surface,Snake.color,snakebits, 3)
 
 def main():
+    screen_width = 400
+    screen_height = 300
     pygame.init()
-    screen = pygame.display.set_mode((400, 300))
+    screen = pygame.display.set_mode((screen_width, screen_height))
     done = False
     snake_color = (255,0,255)
-    snake = Snake(snake_color)
+    snake_speed = 5
     clock = pygame.time.Clock()
-    food = [(100,100), (50,50)]
+    food = [(200,200)]
     input_direction = "right"
+    snake = Snake(snake_color, start_speed=snake_speed)
     while not done:
+
         screen.fill((0,0,0))
 
         if snake.pos[0] in [snake.pos[x] for x in range(1,len(snake.pos))]:
             break
 
         if snake.pos[0] in food:
-            snake.grow_snake(10)
-
+            snake.grow_snake(snake_speed*2)
+            food.pop()
+            food.append(((randint(10, screen_width)//snake_speed)*snake_speed,(randint(10,(screen_height-10))//snake_speed) * snake_speed))
+            print(food)
         for food_pos in food:
             draw_food(screen, food_pos)
+
         moved = False
         for event in pygame.event.get():
 
@@ -43,8 +50,11 @@ def main():
                     input_direction = "left"
                 if event.key == pygame.K_d:
                     input_direction = "right"
-                moved = True
-                snake.move(input_direction)
+
+                if snake.direction != input_direction:
+                    moved = True
+                    snake.move(input_direction)
+
         if not moved:
             snake.move(snake.direction)
         draw_snake(screen,snake)
