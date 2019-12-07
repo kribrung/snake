4ec4a517abfe05830881b73478b3e0e9dea5a30b
kristofferbrungot <kribrung@stud.ntnu.no>
2019-12-07 17:24:09 +0100

    add menu file and items file, start button class

---
+++ graphics/items.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Button()
+    def __init__(self, surface, color, height, width, position):
+
+        self.width = width
+        self.height = height
+        self.position = position
+        self.base_color = color
+        self.surface = surface
+
+    def hovered(self):
+        pass
+    def pressed(self):
+        pass
